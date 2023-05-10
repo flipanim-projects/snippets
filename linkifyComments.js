6a2ef2f053c82af6ecd047c084c7c7868d3a49b6
pr <pineapplerind.info@gmail.com>
2023-05-10 09:20:26 -0400

    more explanatory comments

--- linkifyComments.js
+++ linkifyComments.js
@@ -5,19 +5,20 @@
     const comments = document.querySelectorAll('.commentText');
     for (const comment of comments) {
         let linkified = linkify(comment.innerHTML)
-        comment.innerHTML = ``;
-        comment.append(linkified);
+        comment.innerHTML = ``;    // erase comment content
+        comment.append(linkified); // and set it to the linkified version instead
     }
 
     function linkify(inputText) {
-        // URLs starting with http://, https://, or ftp://
+        // RegEx pattern to match URLs starting with http://, https://, or ftp://
         var httpReplacePattern = /(\b(https?|ftp):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/gim;
+        // list of links found in the text
         let links = inputText.match(httpReplacePattern);
         if (!links) return inputText;
 
         const textNode = document.createDocumentFragment();
         const textWithoutLinks = inputText.split(httpReplacePattern);
-
+        // insert a clickable link in place of each link
         for (const [i, link] of links.entries()) {
             const linkElement = document.createElement("a");
             linkElement.href = link;
